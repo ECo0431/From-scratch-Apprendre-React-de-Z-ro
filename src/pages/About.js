a165fa99dabd4796dee51a69e67ac4a481179e5b
Elie Coffec <eliecoffec@gmail.com>
2021-11-18 15:01:52 +0100

    init

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo/>
+            <h1>À propos</h1>
+            <br/>
+            <p>Lorem60</p>
+            <br/>
+            <p>lorem140</p>
+        </div>
+    );
+};
+
+export default About;
